4ce1b51425f5c7e12af9b368d032a02a20b42977
kmannai <khoubeibmannai@outlook.com>
2024-10-18 19:31:18 +0100

    unit tests with mockito

--- src/test/java/tn/esprit/tpfoyer17/BlocTests.java
+++ src/test/java/tn/esprit/tpfoyer17/BlocTests.java
@@ -1,12 +1,12 @@
 package tn.esprit.tpfoyer17;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.junit.jupiter.MockitoExtension;
 import tn.esprit.tpfoyer17.entities.Bloc;
 import tn.esprit.tpfoyer17.repositories.BlocRepository;
 import tn.esprit.tpfoyer17.services.BlocService;
@@ -17,8 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
-@Slf4j
+@ExtendWith(MockitoExtension.class)
 public class BlocTests {
 
     @InjectMocks
@@ -58,8 +57,6 @@ public class BlocTests {
     public void testUpdateBloc() {
         bloc.setCapaciteBloc(120);
         when(blocRepository.save(bloc)).thenReturn(bloc);
-        when(blocRepository.findById(bloc.getIdBloc())).thenReturn(Optional.of(bloc));
-
         Bloc updatedBloc = blocService.updateBloc(bloc);
         assertNotNull(updatedBloc);
         assertEquals(120, updatedBloc.getCapaciteBloc());
