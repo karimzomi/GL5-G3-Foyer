860783a3dc600f1a8e29dbb3b41220012d00951e
kmannai <khoubeibmannai@outlook.com>
2024-10-18 19:17:17 +0100

    unit tests with mockito

--- src/test/java/tn/esprit/tpfoyer17/BlocTests.java
+++ src/test/java/tn/esprit/tpfoyer17/BlocTests.java
@@ -1,37 +1,67 @@
 package tn.esprit.tpfoyer17;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.tpfoyer17.entities.Bloc;
-import tn.esprit.tpfoyer17.entities.Etudiant;
-import tn.esprit.tpfoyer17.services.IBlocService;
-import tn.esprit.tpfoyer17.services.IEtudiantService;
+import tn.esprit.tpfoyer17.repositories.BlocRepository;
+import tn.esprit.tpfoyer17.services.BlocService;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @Slf4j
 public class BlocTests {
 
-    @Autowired
-    IBlocService blocService;
+    @InjectMocks
+    BlocService blocService;
 
-    @Test
-    public void testBloc() {
-        Bloc bloc = Bloc.builder()
+    @Mock
+    private BlocRepository blocRepository;
+
+    private Bloc bloc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        bloc = Bloc.builder()
                 .nomBloc("Bloc A")
                 .capaciteBloc(100)
                 .build();
-        blocService.addBloc(bloc);
-        Bloc bloc1 = blocService.getBlocById(bloc.getIdBloc());
-        assertNotNull(bloc1);
-        assertEquals(bloc.getIdBloc(), bloc1.getIdBloc());
-        assertEquals(bloc.getNomBloc(), bloc1.getNomBloc());
-        assertEquals(bloc.getCapaciteBloc(), bloc1.getCapaciteBloc());
-        blocService.deleteBloc(bloc.getIdBloc());
+    }
+
+    @Test
+    public void testCreateBloc() {
+        when(blocRepository.save(bloc)).thenReturn(bloc);
+        Bloc createdBloc = blocService.addBloc(bloc);
+        assertNotNull(createdBloc);
+        assertEquals(bloc, createdBloc);
+    }
+
+    @Test
+    public void testReadBloc() {
+        when(blocRepository.findById(bloc.getIdBloc())).thenReturn(Optional.of(bloc));
+        Bloc foundBloc = blocService.getBlocById(bloc.getIdBloc());
+        assertNotNull(foundBloc);
+        assertEquals(bloc, foundBloc);
+    }
+
+    @Test
+    public void testUpdateBloc() {
+        bloc.setCapaciteBloc(120);
+        when(blocRepository.save(bloc)).thenReturn(bloc);
+        when(blocRepository.findById(bloc.getIdBloc())).thenReturn(Optional.of(bloc));
+
+        Bloc updatedBloc = blocService.updateBloc(bloc);
+        assertNotNull(updatedBloc);
+        assertEquals(120, updatedBloc.getCapaciteBloc());
     }
 }
