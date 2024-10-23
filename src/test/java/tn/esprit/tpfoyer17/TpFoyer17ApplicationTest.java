42e0e5e6ac1776698acb639114d718b3207e8a0b
Karim Zomita <zomitaka@gmail.com>
2024-10-23 15:51:31 +0100

    rename app test (exclude it)

--- src/test/java/tn/esprit/tpfoyer17/TpFoyer17ApplicationTest.java
+++ src/test/java/tn/esprit/tpfoyer17/TpFoyer17ApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TpFoyer17ApplicationTests {
+class TpFoyer17ApplicationTest {
 
     @Test
     void contextLoads() {
