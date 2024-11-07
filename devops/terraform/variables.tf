b5ee2a415e1621ed772e14e7bde4d86b1c8876e6
H_Ilyes <82682523+ilyes384@users.noreply.github.com>
2024-11-07 19:42:56 +0100

    update config-terraform

--- devops/terraform/variables.tf
+++ devops/terraform/variables.tf
@@ -13,19 +13,19 @@ variable "cluster_name" {
 variable "subnet_ids" {
   description = "IDs des sous-réseaux"
   type        = list(string)
-  default     = ["subnet-01d5c7aa316de9889", "subnet-062e256c91cc8b4d7"] # Valeurs par défaut
+  default     = ["subnet-07dc97dd9773d4ece", "subnet-02999fde71944e009"] # Valeurs par défaut
 }
 
 variable "role_arn" {
   description = "ARN du rôle IAM pour EKS"
   type        = string
-  default     = "arn:aws:iam::603379414625:role/LabRole" # Valeur par défaut
+  default     = "arn:aws:iam::832695845398:role/LabRole" # Valeur par défaut
 }
 
 variable "vpc_id" {
   description = "L'ID du VPC pour le cluster EKS"
   type        = string
-  default     = "vpc-06e01eb853c84b324" # Remplacez par votre ID de VPC réel
+  default     = "vpc-0b83bacf0e8efb093" # Remplacez par votre ID de VPC réel
 }
 
 variable "vpc_cidr" {
