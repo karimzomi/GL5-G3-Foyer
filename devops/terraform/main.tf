dba76e699b9897f0d3104f2e3e47500bcbd68e93
kmannai <khoubeibmannai@outlook.com>
2024-11-08 23:07:41 +0100

    Terraform config

--- devops/terraform/main.tf
+++ devops/terraform/main.tf
@@ -2,86 +2,19 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = var.vpc_cidr # Utilisation de la variable pour le CIDR
-}
-
-resource "aws_security_group" "eks_cluster_sg" {
-  name        = "eks-cluster-sg-${var.cluster_name}"
-  description = "Security group for EKS cluster ${var.cluster_name}"
-  vpc_id      = var.vpc_id # Utilisation de la variable pour l'ID du VPC
-
-  ingress {
-    from_port   = 8082
-    to_port     = 8082
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 30000
-    to_port     = 30000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "eks-cluster-sg-${var.cluster_name}"
-  }
-}
-
-resource "aws_security_group" "eks_worker_sg" {
-  name        = "eks-worker-sg-${var.cluster_name}"
-  description = "Security group for EKS worker nodes ${var.cluster_name}"
-  vpc_id      = var.vpc_id # Utilisation de la variable pour l'ID du VPC
-
-  ingress {
-    from_port   = 8083
-    to_port     = 8083
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 30000
-    to_port     = 30000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "eks-worker-sg-${var.cluster_name}"
-  }
-}
-
 resource "aws_eks_cluster" "my_cluster" {
   name     = var.cluster_name
   role_arn = var.role_arn
   version  = "1.30"
 
   vpc_config {
-    subnet_ids         = var.subnet_ids
-    security_group_ids = [aws_security_group.eks_cluster_sg.id]
+    subnet_ids = var.subnet_ids
   }
 }
 
 resource "aws_eks_node_group" "my_node_group" {
   cluster_name    = aws_eks_cluster.my_cluster.name
-  node_group_name = "group-node"
+  node_group_name = "node-grp"
   node_role_arn   = var.role_arn
   subnet_ids      = var.subnet_ids
 
@@ -92,4 +25,39 @@ resource "aws_eks_node_group" "my_node_group" {
   }
 
   instance_types = ["t3.medium"]
+}
+
+data "aws_security_group" "eks_cluster_sg" {
+  filter {
+    name   = "tag:aws:eks:cluster-name"
+    values = ["${var.cluster_name}"]
+  }
+  depends_on = [aws_eks_cluster.my_cluster]
+}
+
+resource "aws_security_group_rule" "allow_30000_traffic" {
+  type              = "ingress"
+  from_port         = 30000
+  to_port           = 30000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = data.aws_security_group.eks_cluster_sg.id
+}
+
+resource "aws_security_group_rule" "allow_8082_traffic" {
+  type              = "ingress"
+  from_port         = 32000
+  to_port           = 32000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = data.aws_security_group.eks_cluster_sg.id
+}
+
+resource "aws_security_group_rule" "allow_30001_traffic" {
+  type              = "ingress"
+  from_port         = 32001
+  to_port           = 32001
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = data.aws_security_group.eks_cluster_sg.id
 }
